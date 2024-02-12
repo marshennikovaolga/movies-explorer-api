4137ef6e7a2e7f0e4bf43a257419bd96bc6d1aa9
Olya <116801530+marshennikovaolga@users.noreply.github.com>
2024-02-12 15:28:32 +0300

    Revert "password regex"
    This reverts commit 2ffe063bb148f3c98f8e2fed3a3bd495bb281659.

--- routes/signin.js
+++ routes/signin.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
-const { emailRegex, passwordRegex } = require('../utils/constants');
+const { emailRegex } = require('../utils/constants');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().pattern(emailRegex).required(),
-    password: Joi.string().pattern(passwordRegex).required(),
+    password: Joi.string().required(),
   }),
 }), login);
 
